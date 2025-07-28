a6ffe38137b5554e4504b67df50a0b12dca98727
solidshark91493 <solidshark91493@yahoo.com>
2025-07-28 09:34:44 -0700

    Update cars.js

--- cars.js
+++ cars.js
@@ -1,73 +1,51 @@
 
 window.cars = [
   {
-    "year": 1968,
-    "name": "Custom Camaro",
-    "series": "Original 16",
-    "collectorNumber": 1,
-    "color": "Spectraflame Blue"
+    year: 1968,
+    name: "Custom Cougar",
+    fullName: "Custom Cougar (1968)",
+    series: "Original 16",
+    base: "Metal / Metal",
+    baseCode: "C123",
+    origin: "USA",
+    detailsURL: "https://hotwheels.fandom.com/wiki/Custom_Cougar_(1968)",
+    variants: [
+      {
+        color: "Spectraflame Red",
+        wheels: "Redline",
+        image: "https://static.wikia.nocookie.net/hotwheels/images/4/41/CustomCougar1968-Red.png",
+        valueEstimate: "$100"
+      },
+      {
+        color: "Spectraflame Blue",
+        wheels: "Redline",
+        image: "https://static.wikia.nocookie.net/hotwheels/images/f/f0/CustomCougar1968-Blue.png",
+        valueEstimate: "$120"
+      }
+    ]
   },
   {
-    "year": 1968,
-    "name": "Beatnik Bandit",
-    "series": "Original 16",
-    "collectorNumber": 2,
-    "color": "Spectraflame Red"
-  },
-  {
-    "year": 1969,
-    "name": "Heavy Chevy",
-    "series": "Mainline",
-    "collectorNumber": 3,
-    "color": "Spectraflame Green"
-  },
-  {
-    "year": 1970,
-    "name": "King Kuda",
-    "series": "Mainline",
-    "collectorNumber": 4,
-    "color": "Gold"
-  },
-  {
-    "year": 1971,
-    "name": "Evil Weevil",
-    "series": "Mainline",
-    "collectorNumber": 5,
-    "color": "Orange"
-  },
-  {
-    "year": 1972,
-    "name": "Snake II",
-    "series": "Mainline",
-    "collectorNumber": 6,
-    "color": "Red"
-  },
-  {
-    "year": 1973,
-    "name": "Firebird Funny Car",
-    "series": "Mainline",
-    "collectorNumber": 7,
-    "color": "White"
-  },
-  {
-    "year": 1974,
-    "name": "Ramblin' Wrecker",
-    "series": "Mainline",
-    "collectorNumber": 8,
-    "color": "White/Blue"
-  },
-  {
-    "year": 1975,
-    "name": "Buzz Off",
-    "series": "Mainline",
-    "collectorNumber": 9,
-    "color": "Yellow"
-  },
-  {
-    "year": 1976,
-    "name": "Street Rodder",
-    "series": "Mainline",
-    "collectorNumber": 10,
-    "color": "Purple"
+    year: 1970,
+    name: "Heavy Chevy",
+    fullName: "Heavy Chevy (1970)",
+    series: "Spoilers",
+    base: "Metal / Plastic",
+    baseCode: "D456",
+    origin: "USA",
+    detailsURL: "https://hotwheels.fandom.com/wiki/Heavy_Chevy_(1970)",
+    variants: [
+      {
+        color: "Spectraflame Green",
+        wheels: "Redline",
+        image: "https://static.wikia.nocookie.net/hotwheels/images/b/bc/HeavyChevy1970-Green.png",
+        valueEstimate: "$150"
+      },
+      {
+        color: "Spectraflame Purple",
+        wheels: "Redline",
+        image: "https://static.wikia.nocookie.net/hotwheels/images/9/90/HeavyChevy1970-Purple.png",
+        valueEstimate: "$200"
+      }
+    ]
   }
 ];
